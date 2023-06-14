2fd7021e8d0755630e7121fca6d9f844cf53c321
Brad Ziegler <bradleyziegler72@gmail.com>
2023-06-14 01:59:49 -0400

    Added feature to be able to launch the ball.

--- Assets/Scripts/BallHandler.cs
+++ Assets/Scripts/BallHandler.cs
@@ -1,29 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.InputSystem;
 
 public class BallHandler : MonoBehaviour {
     [SerializeField] private Rigidbody2D currentBallRigidbody;
+    [SerializeField] private SpringJoint2D currentBallSpringJoint;
+    [SerializeField] private float detachDelay = 1f;
 
     private Camera mainCamera;
+    private bool isDragging;
 
     void Start() {
         mainCamera = Camera.main;
     }
 
     void Update() {
+        if (currentBallRigidbody == null) { return; }
+
         if (!Touchscreen.current.primaryTouch.press.isPressed) {
-            currentBallRigidbody.isKinematic = false;
+            if (isDragging) {
+                LaunchBall();
+            }
+            isDragging = false;
             return;
         }
 
+        isDragging = true;
         currentBallRigidbody.isKinematic = true;
-        
         Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
-
         Vector3 worldPosition = mainCamera.ScreenToWorldPoint(touchPosition);
-
         currentBallRigidbody.position = worldPosition;
+    }
+
+    private void LaunchBall() {
+        currentBallRigidbody.isKinematic = false;
+        currentBallRigidbody = null;
+
+        Invoke(nameof(DetachBall), detachDelay);
+    }
+
+    private void DetachBall() {
+        currentBallSpringJoint.enabled = false;
+        currentBallSpringJoint = null;
     }
 }
